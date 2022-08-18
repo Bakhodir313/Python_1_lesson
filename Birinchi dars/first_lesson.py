a9d5e2b0a5670dab8c132d311c2d7a673584811d
Bakhodir313 <111485551+Bakhodir313@users.noreply.github.com>
2022-08-18 11:33:57 +0500

    First lesson in a Python

---
+++ Birinchi dars/first_lesson.py
@@ -0,0 +1,10 @@
+
+#print - kalit so'zi - chop etish degan ma'noni bildirib
+
+print("Hello world")
+print("Men Yuldashev Bakhodir", 2021, "- yilda tug'ilganman")
+print("Zurmisilar")
+print("""Qashqadaryo viloyati, 
+Qarshi shahri, 
+Qarshi muhandislik-iqtisodiyot instituti, 
+IT markazi""")
---
+++ Ikkinchi dars/Variable.py
@@ -0,0 +1,10 @@
+# O'zgaruvchilar
+
+ism = "Bakhodir"
+yosh = 21
+print("Mening ismim ", ism ,"yoshim esa", yosh)
+
+son1 = 10
+son2 = 15
+yichim = (son1 + son2)**2
+print(yichim)
