b9f7631c1fa99045b143bdb04566c760308fbb21
Bakhodir313 <111485551+Bakhodir313@users.noreply.github.com>
2022-08-23 11:59:45 +0500

    for operatori

---
+++ Uchinchi dars/input.py
@@ -0,0 +1,5 @@
+a = int(input("Birinchi sonno kiriting ="))
+b = int(input("Ikkinchi sonni kiriting ="))
+qoshish = a + b    
+
+print(qoshish)
---
+++ for.py
@@ -0,0 +1,9 @@
+mashina = ["nexia","malibu","cobalt","rasvon"]
+Ismlar = ["Abbos","Baxodir","Nodir"]
+
+for bur in Ismlar:
+    print("Salom", Ismlar[0])
+
+    sonlar = list(range(1,50))
+    for son in sonlar:
+        print(son*2)
