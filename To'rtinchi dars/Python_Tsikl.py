e1b99a01e039b6b3a8bbcbc246273198f5ff8bd3
Bakhodir313 <111485551+Bakhodir313@users.noreply.github.com>
2022-08-25 11:23:38 +0500

    to'rtinchi dars

---
+++ To'rtinchi dars/Python_Tsikl.py
@@ -0,0 +1,12 @@
+#sonlar_kvadrati = []
+#sonlar = list(range(11))
+#for son in sonlar:
+    #sonlar_kvadrati.append(son**2)
+    #print(sonlar)
+    #print(sonlar_kvadrati)
+
+dostlar = []
+print("5 ta do'stingiz kim? =")
+for a in range(5):
+    dostlar.append(input(f"{a+1}-dostingizni kiriting =" ))
+    print(dostlar)
---
+++ To'rtinchi dars/Python_if_else.py
@@ -0,0 +1,14 @@
+# yosh = int(input("Yoshingiz nechchida "))
+# if yosh > 18:
+#     print("Passport olishingiz mumkin")
+# if yosh == 18:
+#     print("Passport olishga yoshingiz to'g'rin keladi  ")
+# if yosh < 18:
+#     print("Passport olishga ruxsat yo'q  ")
+
+ismlar = ["Aziz", "Azamat"]
+ism = str(input('Ismingizn kiriting = '))
+if ism == ismlar:
+    print("Siz ro'yxatda borsiz")
+else:
+    print("Bunday ism ro'xatda yo'q")
